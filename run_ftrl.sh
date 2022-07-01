9c7d018e4252e46810354cc60a776c111e726f11
CANVOLCANO <1819393662@qq.com>
2022-07-01 21:11:24 +0800

    update cleaned code

--- run_ftrl.sh
+++ run_ftrl.sh
@@ -118,63 +118,63 @@ nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv3 --type=yandex&
 
 nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv3 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=synthetic1& 
+nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=synthetic1& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=synthetic2& 
+nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=synthetic2& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=0 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=1 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=2 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=3 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=4 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=5 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=6 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=7 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=8 --data=adv4 --type=yandex& 
 
-# nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=yandex& 
+nohup python ./main.py  --algo=PBMFTRL --seed=9 --data=adv4 --type=yandex& 
 
